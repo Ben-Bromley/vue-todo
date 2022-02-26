37b25739c68d44dd3075c29c250e15f2ed709cc7
Ben Bromley <ben.b@reviews.io>
2022-02-26 20:32:22 +0000

    initial upload w/todo-app

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">
+        <img
+          alt="Vue logo"
+          class="logo"
+          src="@/assets/logo.svg"
+          width="30"
+          height="30"
+        />
+      </RouterLink>
+      <div>
+        <!-- <RouterLink to="/">Home</RouterLink> -->
+        <RouterLink to="/todo">Todo</RouterLink>
+        <RouterLink to="/calculator">Calculator</RouterLink>
+      </div>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+#app {
+  max-width: 860px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+header nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: var(--color-background);
+  margin-bottom: 50px;
+}
+
+a {
+  color: var(--color-text);
+  text-decoration: none;
+  margin-right: 20px;
+}
+
+a:hover {
+  opacity: 0.7;
+}
+</style>
