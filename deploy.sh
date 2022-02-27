f90af63688e4a11ba92d1805b024edee6de411ea
Ben Bromley <ben.b@reviews.io>
2022-02-27 14:57:43 +0000

    fix deploy command, maybe

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env sh
+
 # abort on errors
 set -e
 # build
 npm run build
+
 # navigate into the build output directory
 cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:ben-bromley/vuetilities.git master:gh-pages
+git push -f --set-upstream git@github.com:Ben-Bromley/vuetilities.git master:gh-pages
 cd -
