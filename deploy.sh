335a7a20facd94a69d23857a0b14ac5128c7b985
Ben Bromley <ben.b@reviews.io>
2022-02-28 22:02:58 +0000

    add descriptive comment

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,7 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
+
+# had to add ssh key, include --set-upstream, and capitalise the letters of my name for URI precision
 git push -f --set-upstream git@github.com:Ben-Bromley/vuetilities.git master:gh-pages
 cd -
