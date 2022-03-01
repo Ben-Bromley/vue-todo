8ad5b08907072e2960f4b00c9e89e8ca88d3c494
Ben Bromley <ben.b@reviews.io>
2022-03-01 23:36:15 +0000

    add gameover check

--- src/views/PocketSolitaire.vue
+++ src/views/PocketSolitaire.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 const id = ref(0);
 const pegs = ref([]);
 const holes = ref([]);
+const isDraggable = ref(true);
 
 const generateBoard = () => {
   // generate pegs
@@ -59,16 +60,16 @@ const reset = () => {
   generateBoard();
 };
 
+const toggleDragging = () => {
+  isDraggable.value = !isDraggable.value;
+};
+
 // filter pegs to just the one to render
 const getPeg = (holeId) => {
   return pegs.value.filter((peg) => {
     return peg.position === holeId;
   });
 };
-
-onMounted(() => {
-  generateBoard();
-});
 
 const startDrag = (event, peg) => {
   event.dataTransfer.dropEffect = "move";
@@ -79,17 +80,17 @@ const startDrag = (event, peg) => {
 
 const onDrop = (event, newHole) => {
   const pegID = event.dataTransfer.getData("pegID");
-  const peg = pegs.value.find((peg) => peg.id == pegID);
+  const thisPeg = pegs.value.find((peg) => peg.id == pegID);
 
   // get the old hole for old coord reference
-  let oldHole = holes.value.find((hole) => hole.id === peg.position);
+  let oldHole = holes.value.find((hole) => hole.id === thisPeg.position);
 
   // check there's no pegs in that hole already
   let existingPeg = pegs.value.find((altPeg) => altPeg.position == newHole.id);
 
   // check conditions for moving a peg
 
-  // TODO: ONLY ALLOW MOVEMENT IF JUMPING OVER A PEG
+  // TODO: ONLY ALLOW MOVEMENT IF MOVING 2 SPACES
   if (
     (((oldHole.x + 2 == newHole.x || oldHole.x - 2 == newHole.x) &&
       oldHole.y == newHole.y) ||
@@ -97,35 +98,93 @@ const onDrop = (event, newHole) => {
         oldHole.x == newHole.x)) &&
     !existingPeg
   ) {
-    // update peg position, move to new hole
-    peg.position = newHole.id;
-
-
-    // REMOVE PEG
     // average finds peg between jump, relies on the peg movement rules working
-    let holeRef = {x: (oldHole.x + newHole.x) / 2, y: (oldHole.y + newHole.y) / 2}
+    let jumpedHoleCoords = {
+      x: (oldHole.x + newHole.x) / 2,
+      y: (oldHole.y + newHole.y) / 2,
+    };
     // get hole of peg based on coords
     let holeToRemove = holes.value.find((hole) => {
-      return hole.x === holeRef.x && hole.y === holeRef.y 
+      return hole.x === jumpedHoleCoords.x && hole.y === jumpedHoleCoords.y;
     });
-    // get peg based on hole id
-    let pegToRemove = pegs.value.find((peg)=>peg.position == holeToRemove.id)
-    // remove peg by filtering on id
-    pegs.value = pegs.value.filter((peg)=>{
-      return peg.id != pegToRemove.id;
-    })
 
+    // get peg based on hole id
+    let pegToRemove = pegs.value.find((peg) => peg.position == holeToRemove.id);
+
+    // only move peg, if there is another peg to jump over
+    if (pegToRemove) {
+      // update peg position, move to new hole
+      thisPeg.position = newHole.id;
+
+      // remove peg by filtering on id
+      pegs.value = pegs.value.filter((peg) => {
+        return peg.id != pegToRemove.id;
+      });
+    }
   }
 };
 
-// TODO: ADD WATCHER, IF THERE ARE NO POSSIBLE MOVES LEFT, DECLARE GAME OVER
+onMounted(() => {
+  generateBoard();
+});
 
+const checkNeighbourPegs = (checkPeg) => {
+  let adjacentPegCount = 0;
+  let hasNeighbourPegs = false;
+
+  // for a given peg, check all of the pegs
+  pegs.value.forEach((tempPeg, key) => {
+    // get the temp hole for a peg (for comparison)
+    let tempHole = holes.value.find((hole) => hole.id === tempPeg.position);
+    // get the hole for the peg we're checking
+    let checkHole = holes.value.find((hole) => hole.id === checkPeg.position);
+
+    // if there's a peg adjacent to another peg, return true
+    if (
+      (tempHole.y === checkHole.y - 1 && tempHole.x === checkHole.x) ||
+      (tempHole.y === checkHole.y + 1 && tempHole.x === checkHole.x) ||
+      (tempHole.x === checkHole.x - 1 && tempHole.y === checkHole.y) ||
+      (tempHole.x === checkHole.x + 1 && tempHole.y === checkHole.y)
+    ) {
+      // console.log(tempPeg.id + ' is adjacent to ' + checkPeg.id)
+      adjacentPegCount++;
+      hasNeighbourPegs = true;
+    }
+  });
+  return [hasNeighbourPegs, adjacentPegCount];
+};
+
+watch(pegs, () => {
+  let gameOver = true;
+
+  pegs.value.forEach((checkPeg) => {
+    // check if peg has neighbour pegs
+    let [hasNeighbourPegs, adjacentPegCount] = checkNeighbourPegs(checkPeg);
+    if (hasNeighbourPegs) {
+      gameOver = false;
+      // console.log(`Peg ${checkPeg.id} has ${adjacentPegCount} adjacent pegs`);
+    }
+  });
+  // console.log("---------- END ----------");
+
+  if (gameOver === true) {
+    // end game and display modal
+    setTimeout(() => {
+      alert("GAME OVER");
+    }, 500);
+  }
+});
 // TODO: add modal with score and streaks and stuff (wordle style)
 
-// when dropped, set the item coordinates to the location of that div
+// TODO: USE ARRAY TO TRACK GAME STATE, ADD UNDO BUTTON (ARRAY.POP)
 </script>
 <template>
-  <button @click="reset()">Reset</button>
+  <section>
+    <div>
+      <h1 align="center">Pocket Solitaire</h1>
+      <h3 align="center">(desktop only)</h3>
+    </div>
+  </section>
   <section id="peg-grid">
     <span id="c1" class="corner" />
     <span id="c2" class="corner" />
@@ -139,36 +198,47 @@ const onDrop = (event, newHole) => {
       @dragenter.prevent
       @dragover.prevent
     >
-      <!-- {{ hole.x }},{{ hole.y }} -->
       <div
         v-for="peg in getPeg(hole.id)"
         :key="peg.id"
         class="pin"
-        draggable="true"
+        :draggable="isDraggable"
         @dragstart="startDrag($event, peg)"
       ></div>
     </div>
   </section>
+  <section id="game-buttons">
+    <button @click="reset()">Reset</button>
+    <button @click="toggleDragging()">
+      {{ isDraggable ? "Pause" : "Play" }}
+    </button>
+  </section>
 </template>
 <style scoped>
+@import "../assets/app.css";
+
+h1 {margin-bottom: -10px;}
+h3 {margin-bottom: 30px; font-weight: 300;}
 section#peg-grid {
   display: grid;
-  grid-template-columns: repeat(7, 60px);
+  grid-template-columns: repeat(7, 40px);
   justify-items: center;
-  grid-gap: 25px;
+  grid-gap: 20px;
   font-size: 0.8rem;
+  margin: 0 auto;
+  width: min-content;
 }
 .hole {
-  height: 60px;
-  width: 60px;
+  height: 40px;
+  width: 40px;
   background: var(--color-background-mute);
   border-radius: 50%;
-  padding: 7px;
+  padding: 5px;
 }
 .pin {
   height: 100%;
   width: 100%;
-  background: rgb(139, 139, 0);
+  background: var(--color-peg);
   border-radius: 50%;
 }
 
@@ -200,5 +270,13 @@ section#peg-grid {
   grid-row-end: 8;
   grid-column-start: 6;
   grid-column-end: 8;
+}
+
+section#game-buttons {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 10px;
+  max-width: 350px;
+  margin: 40px auto;
 }
 </style>
