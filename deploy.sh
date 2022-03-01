6d71c826e3699b3325792eff97fd6e1a3fb16342
Ben Bromley <ben.b@reviews.io>
2022-03-01 23:37:20 +0000

    automate 404 generation in deploy command

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,20 @@ npm run build
 # navigate into the build output directory
 cd dist
 
+# the block below duplicate index.html into a 404 page
+# this ensures that navigating directly to a sub page or refreshing a sub page works
+
+# copy the contents of index.html
+fallback=$(<index.html)
+# paste contents into 404.html
+echo $fallback > 404.html
+
+
+# do the git stuff
 git init
 git add -A
 git commit -m 'deploy'
 
-# had to add ssh key, include --set-upstream, and capitalise the letters of my name for URI precision
+# to get this working, I had to add an ssh key, include --set-upstream, and capitalise the letters of my name for URI precision
 git push -f --set-upstream git@github.com:Ben-Bromley/vuetilities.git master:gh-pages
 cd -
