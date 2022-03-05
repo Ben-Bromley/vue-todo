21669779aad9c4ec762f014668f49ca0d65d8a1d
Ben Bromley <ben.b@reviews.io>
2022-03-05 19:46:41 +0000

    extract individual task to component

---
+++ src/components/utilities/todo/todo.vue
@@ -0,0 +1,36 @@
+<script setup>
+const props = defineProps(["todo"]);
+</script>
+<template>
+  <li class="todo-item">
+    <button @click="$emit('complete-task')">✅</button>
+    <span :class="{ complete: props.todo.done }">{{ props.todo.text }}</span>
+    <button @click="$emit('remove-task')" style="margin-left: auto">❌</button>
+  </li>
+</template>
+<style scoped>
+@import url("@/assets/app.css");
+
+li.todo-item {
+  list-style: none;
+  width: 100%;
+  display: flex;
+  align-items: center;
+}
+
+button,
+li {
+  font-size: calc(2vmax + 0.4rem);
+}
+
+button {
+  background: none;
+  border: none;
+  padding: 5px 20px;
+}
+
+.complete {
+  text-decoration: line-through;
+  opacity: 0.5;
+}
+</style>
